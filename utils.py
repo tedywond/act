e80e5d4df328dd240639f2f46d9c2c85b49bcc50
Tewodros W. Ayalew <tewodrosayalew@uchicago.edu>
2024-08-05 15:07:06 -0500

    rollback

--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ class EpisodicDataset(torch.utils.data.Dataset):
             else:
                 start_ts = np.random.choice(episode_len)
             # get observation at start_ts only
-            # qpos = root['/observations/qpos'][start_ts]
+            qpos = root['/observations/qpos'][start_ts]
             qvel = root['/observations/qvel'][start_ts]
             image_dict = dict()
             for cam_name in self.camera_names:
@@ -61,8 +61,7 @@ class EpisodicDataset(torch.utils.data.Dataset):
 
         # construct observations
         image_data = torch.from_numpy(all_cam_images)
-        # qpos_data = torch.from_numpy(qpos).float()
-        qvel_data = torch.from_numpy(qvel).float()
+        qpos_data = torch.from_numpy(qpos).float()
         action_data = torch.from_numpy(padded_action).float()
         is_pad = torch.from_numpy(is_pad).bool()
 
@@ -72,26 +71,23 @@ class EpisodicDataset(torch.utils.data.Dataset):
         # normalize image and change dtype to float
         image_data = image_data / 255.0
         action_data = (action_data - self.norm_stats["action_mean"]) / self.norm_stats["action_std"]
-        # qpos_data = (qpos_data - self.norm_stats["qpos_mean"]) / self.norm_stats["qpos_std"]
-        qvel_data = (qvel_data - self.norm_stats["qpos_mean"]) / self.norm_stats["qpos_std"]
+        qpos_data = (qpos_data - self.norm_stats["qpos_mean"]) / self.norm_stats["qpos_std"]
 
-        return image_data, qvel_data, action_data, is_pad
+        return image_data, qpos_data, action_data, is_pad
 
 
 def get_norm_stats(dataset_dir, num_episodes):
-    all_qvel_data = []
+    all_qpos_data = []
     all_action_data = []
     for episode_idx in range(num_episodes):
         dataset_path = os.path.join(dataset_dir, f'episode_{episode_idx}.hdf5')
         with h5py.File(dataset_path, 'r') as root:
-            # qpos = root['/observations/qpos'][()]
+            qpos = root['/observations/qpos'][()]
             qvel = root['/observations/qvel'][()]
             action = root['/action'][()]
-        # all_qpos_data.append(torch.from_numpy(qpos))
-        all_qvel_data.append(torch.from_numpy(qvel))
+        all_qpos_data.append(torch.from_numpy(qpos))
         all_action_data.append(torch.from_numpy(action))
-    # all_qpos_data = torch.stack(all_qpos_data)
-    all_qvel_data = torch.stack(all_qvel_data)
+    all_qpos_data = torch.stack(all_qpos_data)
     all_action_data = torch.stack(all_action_data)
     all_action_data = all_action_data
 
@@ -101,19 +97,13 @@ def get_norm_stats(dataset_dir, num_episodes):
     action_std = torch.clip(action_std, 1e-2, np.inf) # clipping
 
     # normalize qpos data
-    # qpos_mean = all_qpos_data.mean(dim=[0, 1], keepdim=True)
-    # qpos_std = all_qpos_data.std(dim=[0, 1], keepdim=True)
-    
-    # normalize qvel data
-    qvel_mean = all_qvel_data.mean(dim=[0, 1], keepdim=True)
-    qvel_std = all_qvel_data.std(dim=[0, 1], keepdim=True)
-
-    # qpos_std = torch.clip(qpos_std, 1e-2, np.inf) # clipping
-    qvel_std = torch.clip(qvel_std, 1e-2, np.inf) # clipping
+    qpos_mean = all_qpos_data.mean(dim=[0, 1], keepdim=True)
+    qpos_std = all_qpos_data.std(dim=[0, 1], keepdim=True)
+    qpos_std = torch.clip(qpos_std, 1e-2, np.inf) # clipping
 
     stats = {"action_mean": action_mean.numpy().squeeze(), "action_std": action_std.numpy().squeeze(),
-             "qvel_mean": qvel_mean.numpy().squeeze(), "qvel_std": qvel_std.numpy().squeeze(),
-             "example_qvel": qvel}
+             "qpos_mean": qpos_mean.numpy().squeeze(), "qpos_std": qpos_std.numpy().squeeze(),
+             "example_qpos": qpos}
 
     return stats
 
